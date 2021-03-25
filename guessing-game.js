ae86c0f27cd3ebae8adbd552eb1ea88cb4a2c70a
Andru <dr3wwatkins@gmail.com>
2021-03-25 18:14:53 -0500

    Number Randomizer Structuring

--- guessing-game.js
+++ guessing-game.js
@@ -1,12 +1,19 @@
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let secretNumber = 6;
 
-let checkGuess = function(num) {
+function randomInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+let checkGuess = function(num, secretNumber) {
     if (num > secretNumber) {
         console.log('Too High')
         return false;
@@ -20,9 +27,10 @@ let checkGuess = function(num) {
 }
 
 let askGuess = function() {
+    const secretNumber = randomInRange(0, 100);
     rl.question("Enter a guess: ", answer => {
         answer = Number(answer);
-        if (checkGuess(answer)) {
+        if (checkGuess(answer, secretNumber)) {
             rl.close();
         } else {
             askGuess();
